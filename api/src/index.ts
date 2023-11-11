d9145347e4a86480cf2e99df25f35af4176d5fb1
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-11 01:22:34 -0300

    Connection established with mongodb

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        const port = 3001;
 
-app.listen(3001, () => {
-    console.log('Server is running on https://localhost:3001');
-});
+        app.listen(port, () => {
+            console.log(`Server is running on https://localhost:${port}`);
+        });
+    })
+    .catch(() => console.log('Erro ao conectar ao mongo'));
