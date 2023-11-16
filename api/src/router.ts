a6987ab3f6c26675a981071dee911a460b739827
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-16 18:53:12 -0300

    Introducing product characteristics and using express.static in the upload route

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,5 +1,4 @@
 import path from 'node:path';
-
 import { Router } from 'express';
 import multer from 'multer';
 
@@ -15,6 +14,9 @@ const upload = multer({
         destination(req, file, callback) {
             callback(null, path.resolve(__dirname, '..', 'uploads'));
         },
+        filename(req, file, callback) {
+            callback(null, `${Date.now()}-${file.originalname}`);
+        }
     })
 });
 
