2dc9c431318372670abd0c6b6913f5d36a2a96d0
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-19 20:42:23 -0300

    Typing Orders

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,12 +1,55 @@
+import { Order } from "../../types/Order";
 import { OrdersBoard } from "../OrdersBoard";
 import { Container } from "./styles";
+
+const orders: Order[] = [
+  {
+		"_id": "655a951b9e604d55af62ab68",
+		"table": "01",
+		"status": "WAITING",
+		"products": [
+			{
+				"product": {
+					"name": "Camarão",
+					"imagePath": "1700184778146-camarao-empanado.jpg",
+					"price": 35,
+				},
+				"quantity": 1,
+				"_id": "655a951b9e604d55af62ab69"
+			},
+			{
+				"product": {
+					"name": "Suco de Laranja",
+					"imagePath": "1700189969057-suco-de-laranja.png",
+					"price": 13,
+				},
+				"quantity": 2,
+				"_id": "655a951b9e604d55af62ab6a"
+			}
+		],
+	}
+];
 
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard />
-      <OrdersBoard />
-      <OrdersBoard />
+      <OrdersBoard
+        icon="🕒"
+        title="Fila de espera"
+        orders={orders}
+      />
+
+      <OrdersBoard
+        icon="👨🏼‍🍳"
+        title="Em preparação"
+        orders={[]}
+      />
+
+      <OrdersBoard
+        icon="✅"
+        title="Pronto!"
+        orders={[]}
+      />
     </Container>
   )
 }
