4f59f390b1121e76461e51ed90ca74804208dbcd
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-20 00:03:14 -0300

    Add props in Orders, using formatCurrency and using Reduce for calculate Total

--- frontend/src/components/OrderModal/index.tsx
+++ frontend/src/components/OrderModal/index.tsx
@@ -1,5 +1,6 @@
 import closeIcon from '../../assets/images/close-icon.svg'
 import { Order } from '../../types/Order';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 import { ModalBody, OrderDetails, Overlay } from "./styles"
 
@@ -12,6 +13,10 @@ export function OrderModal({ visible, order }: OrderModalProps) {
   if (!visible || !order) {
     return null;
   }
+
+  const total = order.products.reduce((total, { product, quantity }) => {
+    return total + (product.price * quantity);
+  }, 0);
 
   return (
     <Overlay>
@@ -44,7 +49,30 @@ export function OrderModal({ visible, order }: OrderModalProps) {
         <OrderDetails>
           <strong>Itens</strong>
 
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className="item" key={_id}>
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  width="56"
+                  height="28.51"
+                />
 
+                <span className="quantity">{quantity}x</span>
+
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>{formatCurrency(total)}</strong>
+          </div>
         </OrderDetails>
       </ModalBody>
     </Overlay>
