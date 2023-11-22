5c81cd9649c8d5ad7791d5fb90eb1947fb5a1649
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-22 16:43:40 -0300

    Finish Category component

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -5,8 +5,16 @@ import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 
 import { Category, Icon } from './styles';
+import { useState } from 'react';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -14,15 +22,24 @@ export function Categories() {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
 
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )}
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>
+                {category.icon}
+              </Text>
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+
+        );
+      }}
     />
   );
 }
