d2e8b5dbbe07df268e8149c2762925e1d141a2b7
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-28 09:46:55 -0300

    Add QuantityContainer in CartItem

--- mobile/src/components/Cart/index.tsx
+++ mobile/src/components/Cart/index.tsx
@@ -1,5 +1,7 @@
 import { FlatList } from 'react-native';
 import { CartItem } from '../../types/CartItem';
+import { Actions, Item, ProductContainer, Image, QuantityContainer } from './styles';
+import { Text } from '../Text';
 
 interface CartProps {
   cartItems: CartItem[];
@@ -8,7 +10,27 @@ interface CartProps {
 export function Cart({ cartItems }: CartProps) {
   return (
     <FlatList
-      data={[cartItems]}
+      data={cartItems}
+      keyExtractor={cartItem => cartItem.product._id}
+      showsVerticalScrollIndicator={false}
+      renderItem={({ item: cartItem }) => (
+        <Item>
+          <ProductContainer>
+            <Image
+              source={{
+                uri: `http://192.168.15.15:3001/uploads/${cartItem.product.imagePath}`,
+              }}
+            />
+
+            <QuantityContainer>
+              <Text size={14} color="#666">
+                {cartItem.quantity}x
+              </Text>
+            </QuantityContainer>
+          </ProductContainer>
+          <Actions></Actions>
+        </Item>
+      )}
     />
   );
 }
