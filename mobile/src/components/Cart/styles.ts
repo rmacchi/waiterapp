11b34ebc3a3095e62daa544beb898f5fe3b48f44
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-28 10:24:00 -0300

    Align Icons and included margin on QuantityContainer

--- mobile/src/components/Cart/styles.ts
+++ mobile/src/components/Cart/styles.ts
@@ -2,13 +2,18 @@ import styled from 'styled-components/native';
 
 export const Item = styled.View`
   padding: 8px 0;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const ProductContainer = styled.View`
   flex-direction: row;
 `;
 
-export const Actions = styled.View``;
+export const Actions = styled.View`
+  flex-direction: row;
+`;
 
 export const Image = styled.Image`
   width: 48px;
@@ -20,3 +25,5 @@ export const QuantityContainer = styled.View`
   min-width: 20px;
   margin-left: 12px;
 `;
+
+export const ProductDetails = styled.View``;
