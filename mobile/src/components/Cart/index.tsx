fb50736e5fb46596cb6abf7e0cb1e06eb86bf8c2
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-29 00:30:02 -0300

    Refactor with Conditional rendering

--- mobile/src/components/Cart/index.tsx
+++ mobile/src/components/Cart/index.tsx
@@ -14,55 +14,71 @@ interface CartProps {
 export function Cart({ cartItems }: CartProps) {
   return (
     <>
-      <FlatList
-        data={cartItems}
-        keyExtractor={cartItem => cartItem.product._id}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item: cartItem }) => (
-          <Item>
-            <ProductContainer>
-              <Image
-                source={{
-                  uri: `http://192.168.15.15:3001/uploads/${cartItem.product.imagePath}`,
-                }}
-              />
+      {cartItems.length > 0 && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={cartItem => cartItem.product._id}
+          showsVerticalScrollIndicator={false}
+          style={{ marginBottom: 20, maxHeight: 150 }}
+          renderItem={({ item: cartItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={{
+                    uri: `http://192.168.15.15:3001/uploads/${cartItem.product.imagePath}`,
+                  }}
+                />
 
-              <QuantityContainer>
-                <Text size={14} color="#666">
-                  {cartItem.quantity}x
-                </Text>
-              </QuantityContainer>
+                <QuantityContainer>
+                  <Text size={14} color="#666">
+                    {cartItem.quantity}x
+                  </Text>
+                </QuantityContainer>
 
-              <ProductDetails>
-                <Text size={14} weight="600">{cartItem.product.name}</Text>
-                <Text size={14} color="#666" style={{ marginTop: 4 }}>
-                  {formatCurrency(cartItem.product.price)}
-                </Text>
-              </ProductDetails>
-            </ProductContainer>
+                <ProductDetails>
+                  <Text size={14} weight="600">{cartItem.product.name}</Text>
+                  <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                    {formatCurrency(cartItem.product.price)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
 
-            <Actions>
-              <TouchableOpacity style={{ marginRight: 24 }}>
-                <PlusCircle />
-              </TouchableOpacity>
+              <Actions>
+                <TouchableOpacity style={{ marginRight: 24 }}>
+                  <PlusCircle />
+                </TouchableOpacity>
 
-              <TouchableOpacity>
-                <MinusCircle />
-              </TouchableOpacity>
-            </Actions>
-          </Item>
-        )}
-      />
+                <TouchableOpacity>
+                  <MinusCircle />
+                </TouchableOpacity>
+              </Actions>
+            </Item>
+          )}
+        />
+      )}
 
       <Summary>
         <TotalContainer>
-          <Text color="#666">Total</Text>
-          <Text weight="600" size={20}>{formatCurrency(120)}</Text>
-
-          <Button onPress={() => alert('Confirmar pedido')}>
-            Confirmar pedido
-          </Button>
+          {cartItems.length > 0 ? (
+            <>
+              <Text color="#666">Total</Text>
+              <Text weight="600" size={20}>
+                {formatCurrency(120)}
+              </Text>
+            </>
+          ) : (
+            <Text size={16} color="#999">
+              Seu carrinho está vazio
+            </Text>
+          )}
         </TotalContainer>
+
+        <Button
+          onPress={() => alert('Confirmar pedido')}
+          disabled={cartItems.length === 0}
+        >
+            Confirmar pedido
+        </Button>
       </Summary>
     </>
   );
