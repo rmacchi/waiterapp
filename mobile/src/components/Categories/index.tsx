01a8d8bf3d5b9e339d4d01d44a9697ebc0212cac
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-11-30 00:09:15 -0300

    Add props for categories

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -1,13 +1,16 @@
 import { FlatList } from 'react-native';
 
-import { categories } from '../../mocks/categories';
-
 import { Text } from '../Text';
 
-import { Category, Icon } from './styles';
+import { CategoryContainer, Icon } from './styles';
 import { useState } from 'react';
+import { Category } from '../../types/Category';
 
-export function Categories() {
+interface CategoriesProps {
+  categories: Category[];
+}
+
+export function Categories({ categories }: CategoriesProps) {
   const [selectedCategory, setSelectedCategory] = useState('');
 
   function handleSelectCategory(categoryId: string) {
@@ -26,7 +29,7 @@ export function Categories() {
         const isSelected = selectedCategory === category._id;
 
         return (
-          <Category onPress={() => handleSelectCategory(category._id)}>
+          <CategoryContainer onPress={() => handleSelectCategory(category._id)}>
             <Icon>
               <Text opacity={isSelected ? 1 : 0.5}>
                 {category.icon}
@@ -36,7 +39,7 @@ export function Categories() {
             <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
               {category.name}
             </Text>
-          </Category>
+          </CategoryContainer>
 
         );
       }}
