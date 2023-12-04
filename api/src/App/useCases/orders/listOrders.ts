6a3a74d68e2e3cd32ebb54c5580455ca08503316
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-04 00:42:58 -0300

    End project

--- api/src/App/useCases/orders/listOrders.ts
+++ api/src/App/useCases/orders/listOrders.ts
@@ -5,7 +5,7 @@ import { Order } from '../../models/Order';
 export async function listOrders(req: Request, res: Response) {
     try {
         const orders = await Order.find()
-            .sort({ createdAt: -1 })
+            .sort({ createdAt: 1 })
             .populate('products.product');
 
         res.json(orders);
