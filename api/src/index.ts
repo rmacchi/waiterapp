37b8467e26bb0eb20647b5ffa5cb3a30e222e23f
Rafael Macchi <rafaelmacchi199@gmail.com>
2023-12-04 21:08:12 -0300

    Connect api to MongoDB

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,33 +1,53 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import path from 'node:path';
 import http from 'node:http';
 import express from 'express';
-import mongoose from 'mongoose';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
 import { router } from './router';
-
 
 const app = express();
 const server = http.createServer(app);
 export const io = new Server(server);
+dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017')
-    .then(() => {
-        const port = 3001;
+const port = 3001;
 
-        app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Methods', '*');
-            res.setHeader('Access-Control-Allow-Headers', '*');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
-            next();
-        });
-        app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-        app.use(express.json());
-        app.use(router);
+    next();
+});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(express.json());
+app.use(router);
 
-        server.listen(port, () => {
-            console.log(`Server is running on https://localhost:${port}`);
-        });
-    })
-    .catch(() => console.log('Erro ao conectar ao mongo'));
+server.listen(port, () => {
+    console.log(`Server is running on https://localhost:${port}`);
+});
+
+
+const uri = `${process.env.MONGODB_URL}`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db('admin').command({ ping: 1 });
+        console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
